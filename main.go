6d4a1e088254bcad1fbb7190d9d4aa032912ae9f
prateek khened <code.prateekkhened@gmail.com>
2025-09-28 02:05:32 +0530

    reads the lines from the tcp connection with goroutine

--- main.go
+++ main.go
@@ -5,25 +5,47 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	file, err := os.Open("message.txt")
+	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
 
-	buffer := make([]byte, 8)
-
-	for {
-		bytesRead, err := file.Read(buffer)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("read: %s\n", string(buffer[:bytesRead]))
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
 	}
+
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+		defer f.Close()
+		currentLine := ""
+		for {
+			buffer := make([]byte, 8)
+			bytesRead, err := f.Read(buffer)
+			if err != nil {
+				if currentLine != "" {
+					ch <- currentLine
+				}
+				if err == io.EOF {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			parts := strings.Split(string(buffer[:bytesRead]), "\n")
+			for _, part := range parts[:len(parts)-1] {
+				ch <- fmt.Sprintf("%s%s", currentLine, part)
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
+		}
+	}()
+	return ch
 }
