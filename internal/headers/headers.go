9e71c287541e05e79cdd685350668ab7b71868bc
prateek khened <code.prateekkhened@gmail.com>
2025-10-01 10:44:30 +0530

    now we have a parse for the headers with loose implementation with testcases all passing

---
+++ internal/headers/headers.go
@@ -0,0 +1,53 @@
+package headers
+
+import (
+	"errors"
+	"strings"
+)
+
+type Headers map[string]string
+
+const crlf = "\r\n"
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	str := string(data)
+
+	if !strings.Contains(str, crlf) {
+		return 0, false, nil
+	}
+
+	if strings.HasPrefix(str, crlf) {
+		return 2, true, nil
+	}
+
+	i := strings.Index(str, crlf)
+	line := str[:i]
+	c := strings.IndexByte(line, ':')
+
+	if c == -1 {
+		return 0, false, errors.New("invalid colon in the field line")
+	}
+
+	if c > 0 && line[c-1] == ' ' {
+		return 0, false, errors.New("invalid space before colon and after the field-name")
+	}
+
+	key := strings.TrimSpace(line[:c])
+	value := strings.TrimSpace(line[c+1:])
+
+	if key == "" {
+		return 0, false, errors.New("invalid key")
+	}
+
+	h.Set(key, value)
+
+	return i + 2, false, nil
+}
+
+func (h Headers) Set(key, value string) {
+	h[key] = value
+}
